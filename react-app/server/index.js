99ed13875243931f0d277e3f94b437b699dadf9c
Yogesh <myogeshchavan97@gmail.com>
2020-08-13 11:45:11 +0530

    added client side routing support as requested

--- react-app/server/index.js
+++ react-app/server/index.js
@@ -6,6 +6,10 @@ const app = express(); // create express app
 app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
 
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
+
 // start express server on port 5000
 app.listen(5000, () => {
   console.log("server started on port 5000");
